c77e791d3266ec486bb474270c11a37bef12682f
Mhite55 <pierreguyot55@gmail.com>
2023-12-11 16:21:04 +0100

    vue js

---
+++ projet01/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <SquareColor color="#feaa0c"/>
+  <SquareColor color="#fe580c"/>
+  <SquareColor color="#f31c3c"/>
+  <SquareColor color="#f00e0c"/>
+</template>
+
+<script>
+
+import SquareColor from './components/SquareColor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SquareColor
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ projet01/src/components/SquareColor.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="hello">
+      <h1 :style="{color: color}">Ma super couleur {{ color }}!!</h1>
+      <div :style="{backgroundColor: color}" ></div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SquareColor',
+    props: {
+      color: String
+    }
+  }
+  </script>
+  <style scoped>
+    
+  div {
+    margin-bottom: 100px;
+    width: 100vw;
+    height: 20vh;
+  }
+
+  </style>
+  
